a23ab1aaf89a451667a500c85be1bf379dce3a46
Natthakrit Lalert <nutthakit4321@gmail.com>
2023-10-22 21:27:31 +0700

    add docker

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import mongoose from 'mongoose'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
  
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 8080
 
 const app = express()
 
