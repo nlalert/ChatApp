a3fa27fbe8f0250cc1b900a50142f2c1b731e1c0
NigaiNamida <creeper0864@gmail.com>
2023-10-25 16:24:26 +0700

    Changed theme to orange

--- index.js
+++ index.js
@@ -115,7 +115,6 @@ function buildMsg(name, text) {
       time: new Intl.DateTimeFormat('default', {
           hour: 'numeric',
           minute: 'numeric',
-          second: 'numeric'
       }).format(new Date())
   }
 }
