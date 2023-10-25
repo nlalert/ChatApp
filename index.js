68c6dfb3be0b566a371ee53d24c103f22eb84b13
Natthakrit Lalert <nutthakit4321@gmail.com>
2023-10-25 18:30:40 +0700

    format everything

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-import express from 'express'
-import { Server } from "socket.io"
-import path from 'path'
-import { fileURLToPath } from 'url'
-import mongoose from 'mongoose'
+import express from "express";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+import mongoose from "mongoose";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
- 
-const PORT = process.env.PORT || 8080
-const ADMIN = "Admin"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const app = express()
+const PORT = process.env.PORT || 8080;
+const ADMIN = "Admin";
+
+const app = express();
 
 //connect to MongoDB
-mongoose.connect('mongodb+srv://chatuser:GEBBxGJwp67HADQG@cluster0.mx9kas7.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://chatuser:GEBBxGJwp67HADQG@cluster0.mx9kas7.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 //Create schema for message
 const messageSchema = new mongoose.Schema({
@@ -25,99 +28,115 @@ const messageSchema = new mongoose.Schema({
   timestamp: String,
 });
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 const expressServer = app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+  console.log(`listening on port ${PORT}`);
+});
 
 //web socket server
 const io = new Server(expressServer, {
-  cors : {
-    origin: "*"
-  }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-let typing = []
+let typing = [];
 
 //when client connect with server
-io.on('connection', async (socket) => {
-  let userName
-    socket.on('join', async (name) => {
-      userName = name
-      console.log(userName + " connected")
-      try {
-        // Retrieve message history from MongoDB
-        const messages = await Message.find().sort({ timestamp : 1}).exec();
-    
-        // Send message history to the connecting user
-        messages.forEach((message) => {
-          socket.emit('message', {
-            name :`${message.user}`,
-            text : `${message.message}`,
-            time : `${message.timestamp}`});
+io.on("connection", async (socket) => {
+  let userName;
+  socket.on("join", async (name) => {
+    userName = name;
+    console.log(userName + " connected");
+    try {
+      // Retrieve message history from MongoDB
+      const messages = await Message.find().sort({ timestamp: 1 }).exec();
+
+      // Send message history to the connecting user
+      messages.forEach((message) => {
+        socket.emit("message", {
+          name: `${message.user}`,
+          text: `${message.message}`,
+          time: `${message.timestamp}`,
         });
-      } catch (error) {
-        console.error('Error retrieving message history from MongoDB:', error);
-      }
+      });
+    } catch (error) {
+      console.error("Error retrieving message history from MongoDB:", error);
+    }
 
-      //only to user
-      socket.emit('message', buildMsg(ADMIN, `Welcome to ChatApp ${userName}`))
-  
-      //to all others except user
-      socket.broadcast.emit('message', buildMsg(ADMIN, ` ${userName} has joined the ChatApp`))
+    //only to user
+    socket.emit("message", buildMsg(ADMIN, `Welcome to ChatApp ${userName}`));
 
-    })
+    //to all others except user
+    socket.broadcast.emit(
+      "message",
+      buildMsg(ADMIN, ` ${userName} has joined the ChatApp`)
+    );
+  });
 
-    //Listening for a message event
-    socket.on('message', async (text) => {
-        console.log(userName + " : " + text)
-        const message = new Message({
-          user: userName,
-          message: text,
-          timestamp: new Intl.DateTimeFormat('default', {
-            hour: 'numeric',
-            minute: 'numeric',
-        },"hour12").format(new Date())
-        })
-      
-        try {
-          await message.save();
-          io.emit('message', buildMsg(userName, text))
-        } catch (error) {
-          console.error('Error saving message to MongoDB:', error)
-        }
-    })
+  //Listening for a message event
+  socket.on("message", async (text) => {
+    console.log(userName + " : " + text);
+    const message = new Message({
+      user: userName,
+      message: text,
+      timestamp: new Intl.DateTimeFormat(
+        "default",
+        {
+          hour: "numeric",
+          minute: "numeric",
+        },
+        "hour12"
+      ).format(new Date()),
+    });
 
-    //When user disconnects - to all others
-    socket.on('disconnect', () => {
-      console.log(userName + " disconnected")
-      socket.broadcast.emit('message', buildMsg(ADMIN, ` ${userName} has left the ChatApp`))
-    })
+    try {
+      await message.save();
+      io.emit("message", buildMsg(userName, text));
+    } catch (error) {
+      console.error("Error saving message to MongoDB:", error);
+    }
+  });
 
-    //Listen for activity
-    socket.on('activity', () =>{
-      if(!typing.includes(userName)){
-        typing.push(userName)
-      }
-      console.log(typing)
-      socket.broadcast.emit('activity', userName, typing)
-    })
+  //When user disconnects - to all others
+  socket.on("disconnect", () => {
+    console.log(userName + " disconnected");
+    if (userName) {
+      socket.broadcast.emit(
+        "message",
+        buildMsg(ADMIN, ` ${userName} has left the ChatApp`)
+      );
+    }
+  });
 
-    socket.on('deleteTyping', (name) =>{
-      typing.splice(typing.indexOf(name), 1)
-    })
-})
+  //Listen for activity
+  socket.on("activity", () => {
+    if (!typing.includes(userName)) {
+      typing.push(userName);
+    }
+    console.log(typing);
+    socket.broadcast.emit("activity", userName, typing);
+  });
+
+  socket.on("deleteTyping", (name) => {
+    typing.splice(typing.indexOf(name), 1);
+  });
+});
 
 function buildMsg(name, text) {
   return {
-      name,
-      text,
-      time: new Intl.DateTimeFormat('default', {
-          hour: 'numeric',
-          minute: 'numeric',
-      },"hour12").format(new Date())
-  }
+    name,
+    text,
+    time: new Intl.DateTimeFormat(
+      "default",
+      {
+        hour: "numeric",
+        minute: "numeric",
+      },
+      "hour12"
+    ).format(new Date()),
+  };
 }
--- public/app.js
+++ public/app.js
@@ -1,94 +1,88 @@
-const socket = new io('https://chat-app-2-rxhj.onrender.com/')
+const socket = new io("ws://localhost:8080");
 
-const fontWeb = document.querySelector('.web')
-const bg = document.querySelector('.bg')
-const nameInput = document.querySelector('#name')
-const msgInput = document.querySelector('#message')
-const activity = document.querySelector('.activity')
-const chatDisplay = document.querySelector('.chat-display')
-const formMsg = document.querySelector('.form-msg')
+const fontWeb = document.querySelector(".web");
+const bg = document.querySelector(".bg");
+const nameInput = document.querySelector("#name");
+const msgInput = document.querySelector("#message");
+const activity = document.querySelector(".activity");
+const chatDisplay = document.querySelector(".chat-display");
+const formMsg = document.querySelector(".form-msg");
 formMsg.style.visibility = "hidden";
 chatDisplay.style.visibility = "hidden";
 
 function sendMessage(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (msgInput.value) {
-        socket.emit('message', (msgInput.value))
-        msgInput.value = ""
+        socket.emit("message", msgInput.value);
+        msgInput.value = "";
     }
-    msgInput.focus()
+    msgInput.focus();
 }
 
 function join(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (nameInput.value) {
-        socket.emit('join', (nameInput.value))
-        document.querySelector('.form-join').style.visibility = "hidden";
-        document.querySelector('.bg').style.visibility = "hidden";
-        document.querySelector('.web').style.visibility = "hidden";
+        socket.emit("join", nameInput.value);
+        document.querySelector(".form-join").style.visibility = "hidden";
+        document.querySelector(".bg").style.visibility = "hidden";
+        document.querySelector(".web").style.visibility = "hidden";
         chatDisplay.style.visibility = "visible";
         formMsg.style.visibility = "visible";
     }
 }
 
-document.querySelector('.form-join')
-    .addEventListener('submit', join)
+document.querySelector(".form-join").addEventListener("submit", join);
 
-document.querySelector('.form-msg')
-    .addEventListener('submit', sendMessage)
+document.querySelector(".form-msg").addEventListener("submit", sendMessage);
 
-msgInput.addEventListener('keypress', () => {
-    socket.emit('activity')
-})
+msgInput.addEventListener("keypress", () => {
+    socket.emit("activity");
+});
 
-let lastTime = null
-// Listen for messages 
+let lastTime = null;
+// Listen for messages
 socket.on("message", (data) => {
-    activity.textContent = ""
-    let { name, text, time } = data
-    const li = document.createElement('li')
-    li.className = 'post'
-    if(time == lastTime){
-        lastTime = time
-        time = ""
+    activity.textContent = "";
+    let { name, text, time } = data;
+    const li = document.createElement("li");
+    li.className = "post";
+    if (time == lastTime) {
+        lastTime = time;
+        time = "";
+    } else {
+        lastTime = time;
     }
-    else{
-        lastTime = time
-    }
-    
+
     if (name === nameInput.value) {
-        li.className = 'post--right'
+        li.className = "post--right";
         li.innerHTML = `<div class="post__header">
         <span class="post__header--time">${time}</span> 
         <span class="post__header--name">${name}</span> 
         </div>
-        <div class="post--user post__text">${text}</div>`
-    }
-    else if (name !== nameInput.value && name !== 'Admin') {
-        li.className = 'post--left'
+        <div class="post--user post__text">${text}</div>`;
+    } else if (name !== nameInput.value && name !== "Admin") {
+        li.className = "post--left";
         li.innerHTML = `<div class="post__header">
         <span class="post__header--name">${name}</span> 
         <span class="post__header--time">${time}</span> 
         </div>
-        <div class="post--reply post__text">${text}</div>`
+        <div class="post--reply post__text">${text}</div>`;
+    } else {
+        li.className = "post--center";
+        li.innerHTML = `<div class="post__Admin">${text}</div>`;
     }
-    else {
-        li.className = 'post--center'
-        li.innerHTML = `<div class="post__Admin">${text}</div>`
-    }
-    document.querySelector('.chat-display').appendChild(li)
+    document.querySelector(".chat-display").appendChild(li);
 
-    chatDisplay.scrollTop = chatDisplay.scrollHeight
-})
+    chatDisplay.scrollTop = chatDisplay.scrollHeight;
+});
 
-let activityTimer
-socket.on('activity', (name, typingUser) =>{
-    
-    activity.textContent = `${typingUser} is typing...`
+let activityTimer;
+socket.on("activity", (name, typingUser) => {
+    activity.textContent = `${typingUser} is typing...`;
     //Clear after 1 seconds
-    clearTimeout(activityTimer)
+    clearTimeout(activityTimer);
     activityTimer = setTimeout(() => {
-        socket.emit('deleteTyping', name)
-        activity.textContent = ""
-    }, 2000)
-})
+        socket.emit("deleteTyping", name);
+        activity.textContent = "";
+    }, 2000);
+});
